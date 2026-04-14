d3a20f235a8e3171ea84de96edfa36019161cafd
Matias Julian Martinez <167256482+MatiasJulianMartinez@users.noreply.github.com>
2026-04-14 13:15:28 -0300

    Click tanto en imagen como en boton funcionan

--- app/src/main/java/com/example/a8_4/MainActivity.java
+++ app/src/main/java/com/example/a8_4/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a8_4;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+    /* evento de click sobre boton */
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("app-testing","el click sobre el boton esta funcionando");
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
